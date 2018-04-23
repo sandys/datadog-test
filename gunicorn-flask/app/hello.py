639faf2e847e1c2d90e5e107705c233ddd19f4ff
sandeep srinivasa <sss@lambdacurry.com>
2018-04-24 00:37:39 +0530

    scoutapp commit

--- gunicorn-flask/app/hello.py
+++ gunicorn-flask/app/hello.py
@@ -6,16 +6,25 @@ import socket
 from flask import Flask, jsonify
 import blinker as _
 
-from ddtrace import tracer
-from ddtrace.contrib.flask import TraceMiddleware
 
-from ddtrace import patch_all
-patch_all()
-
+from scout_apm.flask import ScoutApm
 log = logging.getLogger(__name__)
 app = Flask(__name__)
-tracer.configure(hostname='datadog')
-traced_app = TraceMiddleware(app, tracer, service="my-flask-app", distributed_tracing=False)
+
+#from scout_apm.flask.sqlalchemy import instrument_sqlalchemy
+
+# Setup a flask 'app' as normal
+
+## Attaches ScoutApm to the Flask App
+ScoutApm(app)
+
+## Instrument the SQLAlchemy handle
+#instrument_sqlalchemy(db)
+
+# Scout settings
+app.config['SCOUT_MONITOR'] = True
+app.config['SCOUT_KEY']     = "UOoV3CDkZy3fYZYJW4Qs"
+app.config['SCOUT_NAME']    = "A FRIENDLY NAME FOR YOUR APP"
 
 @app.route('/')
 def hello():
